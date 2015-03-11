971cc74a6611eb32543c0765ed4918c5da910a56
Boris Bügling <boris@icculus.org>
2015-03-11 15:21:09 +0100

    Add copyright info for code derived from WebKit

--- Code/CCBufferedImageDecoder.m
+++ Code/CCBufferedImageDecoder.m
@@ -1,10 +1,47 @@
-//
-//  CCBufferedImageDecoder.m
-//  Concorde
-//
-//  Created by Boris Bügling on 10/03/15.
-//  Copyright (c) 2015 Contentful GmbH. All rights reserved.
-//
+/*
+ * CCBufferedImageDecoder.m
+ * Concorde
+ *
+ * Code is derived from WebKit's Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp,
+ * licensed under the following terms.
+ *
+ * Copyright (C) 2006 Apple Inc.
+ *
+ * Portions are Copyright (C) 2001-6 mozilla.org
+ *
+ * Other contributors:
+ *   Stuart Parmenter <stuart@mozilla.com>
+ *
+ * Copyright (C) 2007-2009 Torch Mobile, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Alternatively, the contents of this file may be used under the terms
+ * of either the Mozilla Public License Version 1.1, found at
+ * http://www.mozilla.org/MPL/ (the "MPL") or the GNU General Public
+ * License Version 2.0, found at http://www.fsf.org/copyleft/gpl.html
+ * (the "GPL"), in which case the provisions of the MPL or the GPL are
+ * applicable instead of those above.  If you wish to allow use of your
+ * version of this file only under the terms of one of those two
+ * licenses (the MPL or the GPL) and not to allow others to use your
+ * version of this file under the LGPL, indicate your decision by
+ * deletingthe provisions above and replace them with the notice and
+ * other provisions required by the MPL or the GPL, as the case may be.
+ * If you do not delete the provisions above, a recipient may use your
+ * version of this file under any of the LGPL, the MPL or the GPL.
+ */
 
 #import <stdio.h>
 #import "jpeglib.h"
